d23820fd10d6ecb48fdb32d15a51bdef4d7e9f93
Andrew Cobb <andrew@cobbs-web.com>
2019-11-09 17:04:23 -0500

    testing back end routing 7

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,7 @@ app.use(cors());
 
 //Static Dir
 app.use(express.static(__dirname + '/../dist/gamer-lobby'));
+app.use('/**' .express.static(__dirname + '/../dist/gamer-lobby'));
 console.log('app dir: '+ __dirname+ '/../dist/gamer-lobby');
 
 //Restful API 
@@ -43,7 +44,7 @@ app.listen(port, () => {
 
 // index Route
 app.get('/**', (req, res) => {
-  express.static(__dirname + '/../dist/gamer-lobby')
+  res.sendFile(__dirname+ '/../dist/gamer-lobby/index.html');
 });
 
 
