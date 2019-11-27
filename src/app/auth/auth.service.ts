6a38cccc84a8146df6e9b95427fd08954fe07135
Andrew Cobb <andrew@cobbs-web.com>
2019-11-27 17:51:15 -0500

    new auth script

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,23 +1,113 @@
 import { Injectable } from '@angular/core';
-import { Observable, of} from 'rxjs';
+//import { Observable, of} from 'rxjs';
 import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import {WebAuth} from 'auth0-js';
 import { environment } from './../../environments/environment';
 import { Router } from '@angular/router';
-
+import { tap, catchError, concatMap, shareReplay } from 'rxjs/operators';
+import Auth0Client from '@auth0/auth0-spa-js/dist/typings/Auth0Client';
+import { from, of, Observable, BehaviorSubject, combineLatest, throwError } from 'rxjs';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  auth0Client$ = (from(
+    createAuth0Client({
+      domain: "snowy-term-2316.auth0.com",
+      client_id: "KaGyM3uGjtynqVAmGosbQEmpJmM5cKV1",
+      redirect_uri: `${window.location.origin}`
+    })
+  ) as Observable<Auth0Client>).pipe(
+    shareReplay(1),
+    catchError(err => throwError(err))
+  );
+  isAuthenticated$ = this.auth0Client$.pipe(
+    concatMap((client: Auth0Client) => from(client.isAuthenticated())),
+    tap(res => this.loggedIn = res)
+  );
+  handleRedirectCallback$ = this.auth0Client$.pipe(
+    concatMap((client: Auth0Client) => from(client.handleRedirectCallback()))
+  );
+  private userProfileSubject$ = new BehaviorSubject<any>(null);
+  userProfile$ = this.userProfileSubject$.asObservable();
+  loggedIn: boolean = null;
+
+  constructor(private router: Router) {
+    this.localAuthSetup();
+    this.handleAuthCallback();
+  }
+
+  getUser$(options?): Observable<any> {
+    return this.auth0Client$.pipe(
+      concatMap((client: Auth0Client) => from(client.getUser(options))),
+      tap(user => this.userProfileSubject$.next(user))
+    );
+  }
+
+  private localAuthSetup() {
+    const checkAuth$ = this.isAuthenticated$.pipe(
+      concatMap((loggedIn: boolean) => {
+        if (loggedIn) {
+          return this.getUser$();
+        }
+        return of(loggedIn);
+      })
+    );
+    checkAuth$.subscribe();
+  }
+
+  login(redirectPath: string = '/') {
+    this.auth0Client$.subscribe((client: Auth0Client) => {
+      client.loginWithRedirect({
+        redirect_uri: `${window.location.origin}`,
+        appState: { target: redirectPath }
+      });
+    });
+  }
+
+  private handleAuthCallback() {
+    const params = window.location.search;
+    if (params.includes('code=') && params.includes('state=')) {
+      let targetRoute: string; /
+      const authComplete$ = this.handleRedirectCallback$.pipe(
+        tap(cbRes => {
+          targetRoute = cbRes.appState && cbRes.appState.target ? cbRes.appState.target : '/';
+        }),
+        concatMap(() => {
+          return combineLatest([
+            this.getUser$(),
+            this.isAuthenticated$
+          ]);
+        })
+      );
+      authComplete$.subscribe(([user, loggedIn]) => {
+        this.router.navigate([targetRoute]);
+      });
+    }
+  }
+
+  logout() {
+    this.auth0Client$.subscribe((client: Auth0Client) => {
+      client.logout({
+        client_id: "YOUR_CLIENT_ID",
+        returnTo: `${window.location.origin}`
+      });
+    });
+  }
+
+/*
   token_expires: number;
   user: any;
   token: string;
   loggedIn: boolean = false;
+*/
 
+  /*
   auth0 = new WebAuth({
-      clientID: 'LWR95861fRJLWyuKsyALy584N0JHN4vd',
+      clientID: 'KaGyM3uGjtynqVAmGosbQEmpJmM5cKV1',
       domain: 'snowy-term-2316.auth0.com/login',
       responseType: 'token',
       redirectUri: 'https://shielded-caverns-18893.herokuapp.com',
@@ -76,5 +166,5 @@ export class AuthService {
 
   constructor(private router: Router) { 
     this.getAccessToken();
-  }
+  }//*/
 }
