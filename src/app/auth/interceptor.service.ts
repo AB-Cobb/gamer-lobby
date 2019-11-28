c1eff0d45d0afe4769a959ccf1fb736807ac168f
Andrew Cobb <andrew@cobbs-web.com>
2019-11-28 17:13:35 -0500

    debugging interceptor

--- src/app/auth/interceptor.service.ts
+++ src/app/auth/interceptor.service.ts
@@ -9,12 +9,14 @@ import { Observable, throwError } from 'rxjs';
 })
 export class InterceptorService implements HttpInterceptor  {
   intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
-    return this.authService.getTokenSilently$().pipe(mergeMap(token => {
-      const tokenRequest = req.clone({ setHeaders: {Authorisation: 'Bearer ${token}'}});
-      return next.handle(tokenRequest);
-    }),
-    catchError(err => throwError(err))
-    );
+    if (this.authService.loggedIn){
+      return this.authService.getTokenSilently$().pipe(mergeMap(token => {
+        const tokenRequest = req.clone({ setHeaders: {Authorisation: 'Bearer ${token}'}});
+        return next.handle(tokenRequest);
+      }),
+      catchError(err => throwError(err))
+      );
+    }
   }
   constructor(private authService: AuthService) {
     //do something
