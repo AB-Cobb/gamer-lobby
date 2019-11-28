5beee97276bf278e8d4a6eeed3b5ba7c6145299b
Andrew Cobb <andrew@cobbs-web.com>
2019-11-27 21:03:31 -0500

    added admin tools link

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddPlayerComponent } from '../components/add-player/add-player.component';
-//import { AdminLoginComponent } from '../components/admin-login/admin-login.component';
 import { EditPlayerComponent } from '../components/edit-player/edit-player.component';
 import { GameListComponent } from '../components/game-list/game-list.component';
-import { JoinGameComponent } from '../components/join-game/join-game.component';
 import { PlayerListComponent } from '../components/player-list/player-list.component';
 import { EditGameComponent } from '../components/edit-game/edit-game.component';
 import { AddGameComponent } from '../components/add-game/add-game.component';
@@ -20,9 +18,7 @@ const routes: Routes = [
           {path: 'game-list', component: GameListComponent}, 
           {path: 'edit-game/:id', component: EditGameComponent},
           {path: 'add-game', component: AddGameComponent},
-          {path: 'join-game/:id', component: JoinGameComponent}, 
-          {path: 'player-list', component: PlayerListComponent,
-          canActivate : [AuthGuard]}, 
+          {path: 'player-list', component: PlayerListComponent}, 
     ]}
 ];
 
