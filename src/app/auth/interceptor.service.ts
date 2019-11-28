5cee0d5dcae4d4e15036f6655b96fe838a084e86
Andrew Cobb <andrew@cobbs-web.com>
2019-11-28 16:47:26 -0500

    jwt back end debugging

--- src/app/auth/interceptor.service.ts
+++ src/app/auth/interceptor.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
-import { AuthService } from './auth.service';
+import { AuthService,  } from './auth.service';
+import { mergeMap, catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor  {
   intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
-    return this.authService.getTokenSilently$()
+    return this.authService.getTokenSilently$().pipe(mergeMap(token => {
+      const tokenRequest = req.clone({ setHeaders: {Authorisation: 'Bearer ${token}'}});
+      return next.handle(tokenRequest);
+    }),
+    catchError(err => throwError(err))
+    );
   }
-
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+    //do something
+  }
 }
